7e530843aec0f9fc13ae378b3f337bc66e07c820
Raquel Aoki <raquel.aoki@gmail.com>
2021-04-15 15:08:46 -0700

    Update baselines.py

--- resources/baselines.py
+++ resources/baselines.py
@@ -12,12 +12,19 @@ class methodNAME:
         print('adding fitting part')
         print('should output predictions and some metrics for evaluation')
         self.model = model
+        y_test_pred = pred(X_test) # generic pred function, depends on the method
+        return f1_score(self.y_test, y_test_pred)
 
     def cate(self):
         print('optitional - depends on the methods implementation')
+        # one value per treatment 
+        return estimated_cate
 
 
 """
 Usage:
 Check the notebook with examples of the other baselines
+
+1. Make sure they all receive the same split X_train, X_test, y_train, y_test
+2. Return CATE, return f1_score for testing set
 """
