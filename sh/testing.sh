29756cf32a9977b1dcd316f14097a95e40726fbc
Raquel Aoki <raquel.aoki@gmail.com>
2021-04-20 10:24:40 -0700

    Update testing.sh

--- sh/testing.sh
+++ sh/testing.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #SBATCH --time=00:15:00
 #SBATCH --account=def-ester
-#SBATCH --gres=gpu:p100:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1024M
 
 echo 'Starting ....'
+echo 'not using #SBATCH --gres=gpu:p100:1'
 
 module load python/3.8 cuda cudnn
 
-SOURCEDIR=/M3E2
+SOURCEDIR= $HOME/M3E2
 
-source /env/bin/activate
+source $HOME/M3E2/env/bin/activate
 
 python testing_slurm.py
 
+echo 'SUCESS, NOW LOOK FOR FILE'
+
+#SBATCH -o /slurm/output.%a.out
