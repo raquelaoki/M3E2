ff896652fddc94f87cdbee252c9ef294ab2423be
Raquel Aoki <raquel.aoki@gmail.com>
2021-04-26 10:32:57 -0700

    fix path

--- sh/config2g.sh
+++ sh/config2g.sh
@@ -15,7 +15,7 @@ SOURCEDIR= ~/projects/rrg-ester/raoki/M3E2
 
 source env/bin/activate
 
-python train_models.py config_gwas/config2g.yaml 8 4
+python train_models.py config_copula/config2g.yaml 8 4
 
 echo 'DONE!'
 
