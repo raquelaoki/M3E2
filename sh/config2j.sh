d0e5a520e2567c1395c6723f596a5ed0e2f17e48
Raquel Aoki <raquel.aoki@gmail.com>
2021-04-26 10:03:22 -0700

    config2 files

---
+++ sh/config2j.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --account=rrg-ester
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8000M
+#SBATCH --output=slurm/%x-%j.out
+#SBATCH --gres=gpu:1
+
+echo 'Starting ....'
+
+module load --ignore-cache python/3.7 cuda cudnn
+
+SOURCEDIR= ~/projects/rrg-ester/raoki/M3E2
+
+source env/bin/activate
+
+python train_models.py config_gwas/config2j.yaml 8 4
+
+echo 'DONE!'
+
