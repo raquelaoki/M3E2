198966dae98bbe827aafd58a12aafb80e89435b4
Raquel Aoki <raquel.aoki@gmail.com>
2021-05-20 23:31:31 -0700

    updating path

--- sh/config2e.sh
+++ sh/config2e.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --time=6:00:00
-#SBATCH --account=rrg-ester
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=12000M
@@ -11,7 +10,7 @@ echo 'Starting ....'
 
 module load --ignore-cache python/3.7 cuda cudnn
 
-SOURCEDIR= ~/projects/rrg-ester/raoki/M3E2
+SOURCEDIR= ~/M3E2
 
 source env/bin/activate
 
